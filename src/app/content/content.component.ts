f5740a08fbaf8cb03d28b1b72192262865b9789b
adanfj <ajffj4@gmail.com>
2022-08-14 21:08:19 +0100

    Responsive design

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { faUpload } from '@fortawesome/free-solid-svg-icons';
 import { FileName } from '../files';
@@ -12,22 +12,32 @@ import { FileName } from '../files';
 
 
 export class ContentComponent implements OnInit, OnChanges {
-  @Input() public file!:FileName;
-  @Input() public aspectRatio:string="";
-  @Input() public editing:boolean=false;
-  topic!:string;
-  title!:string;
-  urlSafe!:SafeResourceUrl;
+  @Input() public file!: FileName;
+  @Input() public aspectRatio: string = "";
+  @Input() public editing: boolean = false;
+  @ViewChild("contentVideo") contentVideo!: ElementRef;
+  topic!: string;
+  title!: string;
+  urlSafe!: SafeResourceUrl;
   uploadIcon = faUpload
 
   constructor(public domSanitizer: DomSanitizer) { }
   ngOnChanges(changes: SimpleChanges): void {
     this.urlSafe = this.domSanitizer.bypassSecurityTrustResourceUrl(this.file.getURL());
-    if(this.file.media=="video")this.aspectRatio="aspect-video"
-    else this.aspectRatio=""
+    if (this.file.media == "video") {
+      this.aspectRatio = "aspect-video"
+      if (this.contentVideo) {
+        this.contentVideo.nativeElement.firstChild.src=this.file.getURL()
+        this.contentVideo.nativeElement.load()
+      }
+    }
+    else this.aspectRatio = ""
   }
   ngOnInit(): void {
-    
+
+  }
+  handleTime(e: any) {
+    console.log(e.target);
   }
 
 }
